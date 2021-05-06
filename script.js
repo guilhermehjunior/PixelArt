07809190661752f60d071bbb319f435780d6ca7b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-05 21:02:06 -0300

    fixing some bad coding and renaming some functions and adding pointers to the mouse

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-window.onload = () => {
-
 const pixelBoard = document.querySelector('#pixel-board');
 
-function creatingPixelDivsCollumns(row, colunas) {
+function creatingPixelBoardCollumns(row, colunas) {
   for (let column = 1; column <= colunas; column += 1) {
     const divColumn = document.createElement('div');
     divColumn.classList.add('pixel');
@@ -11,16 +9,16 @@ function creatingPixelDivsCollumns(row, colunas) {
   }
 }
 
-function creatingPixelDivs(linhas) {
+function creatingPixelBoardRows(linhas) {
   for (let row = 1; row <= linhas; row += 1) {
     const divRow = document.createElement('div');
     divRow.classList.add('tr');
     pixelBoard.appendChild(divRow);
-    creatingPixelDivsCollumns(divRow, linhas);
+    creatingPixelBoardCollumns(divRow, linhas);
   }
 }
 
-creatingPixelDivs(5);
+creatingPixelBoardRows(5);
 
 const paletteColors = document.querySelectorAll('.color');
 
@@ -39,12 +37,13 @@ addingEventListenersToPalette(paletteColors);
 
 let pixels = document.querySelectorAll('.pixel');
 
-function addingEventListenersToPixels() {
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', () => {
+function addingEventListenersToPixels(elements) {
+  for (let index = 0; index < elements.length; index += 1) {
+    elements[index].addEventListener('click', (event) => {
       const selColor = document.querySelector('.selected');
-      const BackCol = window.getComputedStyle(selColor, null).getPropertyValue('background-color');
-      pixels[index].style.backgroundColor = BackCol;
+      const bgColor = window.getComputedStyle(selColor, null).getPropertyValue('background-color');
+      const pixelClicked = event.target;
+      pixelClicked.style.backgroundColor = bgColor;
     });
   }
 }
@@ -65,7 +64,7 @@ clearingTheBoard();
 const inputButton = document.querySelector('#generate-board');
 const input = document.querySelector('#board-size');
 
-function boardSize() {
+function changingBoardSize() {
   inputButton.addEventListener('click', () => {
     if (input.value === '') return alert('Board inválido!');
     const divsRowsPixels = pixelBoard.querySelectorAll('.tr');
@@ -74,15 +73,15 @@ function boardSize() {
     }
     if (input.value < 5) input.value = 5;
     if (input.value > 50) input.value = 50;
-    creatingPixelDivs(input.value);
+    creatingPixelBoardRows(input.value);
     pixels = document.querySelectorAll('.pixel');
-    addingEventListenersToPixels();
+    addingEventListenersToPixels(pixels);
   });
 }
 
-boardSize();
+changingBoardSize();
 
-
+window.onload = () => {
   const blackColor = document.querySelector('#first-color');
   blackColor.classList.add('selected');
   // Ideia retirada de https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
